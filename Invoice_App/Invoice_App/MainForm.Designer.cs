1d5bfc031e415b000d9d1c9f8857c84d3a94f66d
Antonia Atanasova <antoniyaata@gmail.com>
2020-03-31 23:30:09 +0300

    Add login form

--- Invoice_App/Invoice_App/MainForm.Designer.cs
+++ Invoice_App/Invoice_App/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Invoice_App
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
--- Invoice_App/Invoice_App/MainForm.cs
+++ Invoice_App/Invoice_App/MainForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Invoice_App
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
         }
